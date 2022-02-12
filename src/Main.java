d11dfc9969950b4cdb694901a335f5b9a4fffca3
Wendy Robins <wendy@therobins.net>
2022-02-12 12:47:59 -0500

    added INVALID_VALUE_MESSAGE

--- src/Main.java
+++ src/Main.java
@@ -17,38 +17,59 @@ print both methods to console.
 
 public class Main {
 
+    private static final String INVALID_VALUE_MESSAGE = "Invalid value";
+
     public static void main(String[] args) {
-        System.out.println(getDurationString(62, 3603));
-        System.out.println(getDurationString( 7205));
+        System.out.println(getDurationString(65L, 45L));
+        System.out.println(getDurationString( 3945L));
+        System.out.println(getDurationString(-41));
+        System.out.println(getDurationString(65, 9));
     }
 
-    public static String getDurationString(long minutes, long seconds){
 
-        if (minutes < 0 || seconds < 0 && seconds > 59){
-            return "Invalid value";
+    private static String getDurationString(long minutes, long seconds) {
+
+        if ((minutes < 0) || (seconds < 0) || (seconds > 59)) {  //validation
+            return INVALID_VALUE_MESSAGE;
         }
         long ttlSeconds = (minutes * 60) + seconds;
 
         //calculate how many hours, minutes, seconds from ttlSeconds
-        long hours = ttlSeconds / 3600;
-        long remainderMinutes = (ttlSeconds % 3600)/60;
-        long remainderSeconds =  ttlSeconds % 60;
+//        long hours = ttlSeconds / 3600;
+//        long remainderMinutes = (ttlSeconds % 3600)/60;
+//        long remainderSeconds =  ttlSeconds % 60;
 
-        String strZero = "";
-        if (hours > 9 || remainderMinutes > 9 || remainderSeconds > 9 ){
-            // include a leading zero in the return if less than double digits
-            strZero = "0";
+        long hours = minutes / 60;
+        long remainderMinutes = minutes % 60;
+
+        // display a leading zero in the return if less than double digits
+        String strHours = hours + "h";
+        if (hours < 10.00) {
+            strHours = "0" + strHours;
         }
-        return strZero + hours + "h " + strZero + remainderMinutes + "m " + strZero + remainderSeconds + "s";
+
+        // display a leading zero in the return if less than double digits
+        String strMinutes = remainderMinutes + "m";
+        if (remainderMinutes < 10.00) {
+            strMinutes = "0" + strMinutes;
+        }
+
+        // display a leading zero in the return if less than double digits
+        String strSeconds = seconds + "s";
+        if (seconds < 10.00) {
+            strSeconds = "0" + strSeconds;
+        }
+            // return joined strings
+        return strHours + " " + strMinutes + " " + strSeconds;
     }
 
-    public static String getDurationString(long seconds){
-        if (seconds < 0){
-            return "Invalid value";
+    private static String getDurationString(long seconds){
+        if (seconds < 0){  //validation
+            return INVALID_VALUE_MESSAGE;
         }
         // calculate how many minutes are in the seconds value and call overloaded method
         long minutes = seconds / 60;
         long remainingSeconds = seconds % 60;
-        return getDurationString(minutes, remainingSeconds);
+        return getDurationString(minutes, remainingSeconds);  //return value from method call
     }
 }
